5c93232b85968f43e9d244cfdfbd3c24079c00cb
Sarah Ahn <sarahahn2004@gmail.com>
2022-10-14 11:52:25 -0600

    HTML1

---
+++ profile.js
@@ -0,0 +1,7 @@
+const favColorBtn = document.querySelector('#color');
+const placeBtn = document.querySelector("#place");
+const favRitText = document.querySelector("#ritual")
+
+function favRitText (event){
+console.log ("Myfavorite color is pink")
+}
